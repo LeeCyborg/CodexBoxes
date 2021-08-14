3a6d27d14118f4367c602a680aa9b5652ea79a52
Linbot <lindywilkins@gmail.com>
2021-08-14 13:58:57 -0400

    box 2 done

--- Box2/Box2.ino
+++ Box2/Box2.ino
@@ -4,55 +4,58 @@
 #include <Servo.h>
 Servo latch;
 const byte BUTTON_PINS[6] = { 2, 3, 4, 5, 6, 7};
-#define NUMPIXELS 1
-#define knife 2
-bool state = false;
+const byte LIGHT_PINS[6] = { 0, 1, 2, 3, 4, 5};
+bool STATE[6] = { false, false, false, false, false, false};
+#define NUMPIXELS 6
+bool done = false;
 
 Adafruit_NeoPixel pixels(NUMPIXELS, 10, NEO_RGB + NEO_KHZ800);
 void setup() {
   pixels.begin();
   latch.attach(9);
   Serial.begin(9600);
-  for (byte i = 0; i < 5; i++){
+  for (byte i = 0; i < 7; i++) {
     pinMode(BUTTON_PINS[i], INPUT_PULLUP);
   }
-  pinMode(knife, INPUT_PULLUP);
   latch.write(0);
 }
 
 void loop() {
-  if (digitalRead(2) == HIGH && state == false) {
-    error();
-    Serial.println("on");
-  } else {
-    flip();
-    Serial.println("off");
+  CheckPins();
+  error();
+  activate();
+}
+
+void CheckPins() {
+  for (int i = 0; i < 6; i++) {
+    if (digitalRead(BUTTON_PINS[i]) == LOW) {
+      STATE[i] = true;
+      Serial.println("flipped");
+    }
   }
 }
 void error() {
-  for (int i = 0; i < NUMPIXELS; i++) {
-    pixels.setPixelColor(i, pixels.Color(0, 255, 0));
-    pixels.show();
-    delay(100);
-  }
-  for (int i = 0; i < NUMPIXELS; i++) {
-    pixels.setPixelColor(i, pixels.Color(0, 0, 0));
-    pixels.show();
-    delay(100);
-  }
-}
-
-void activated() {
-  for (int i = 0; i < NUMPIXELS; i++) {
-    pixels.setPixelColor(i, pixels.Color(255, 0, 0));
-    pixels.show();
+  for (int i = 0; i < 7; i++) {
+    if (STATE[i] == true) {
+      pixels.setPixelColor(i, pixels.Color(0, 255, 0));
+      pixels.show();
+    } else {
+      pixels.setPixelColor(i, pixels.Color(255, 0, 0));
+      pixels.show();
+    }
   }
 }
 
-void flip() {
-  activated();
-  state = true;
-  latch.write(90);
+void activate() {
+  if (STATE[0] == true &&
+      STATE[1] == true &&
+      STATE[2] == true &&
+      STATE[3] == true &&
+      STATE[4] == true &&
+      STATE[5] == true) {
+      latch.write(90);
+      Serial.println("DONE");
+  }
 }
 
 
